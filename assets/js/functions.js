d87f9dd9742b6305bf2750b65fa15df765c3a985
Karthik Meda <karthik@MacBook-Pro.local>
2016-09-18 23:40:40 +0530

    initial

---
+++ assets/js/functions.js
@@ -0,0 +1,40 @@
+
+
+function tweet() {
+    window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent($(".message")));
+}
+
+$(document).ready(function(){
+
+    
+//On Page Load
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); 
+        $('.author').text(post.title);
+        $('.message').html(post.content);     
+      },
+    });
+
+//On Button click
+    $('.new-quote').on('click', function(e) {
+    e.preventDefault();
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var post = data.shift(); 
+        $('.author').text(post.title);
+        $('.message').html(post.content);     
+      },
+      cache: false
+    });
+
+  });
+
+
+
+$('.tweet-quote').on('click', tweet); 
+    console.log($(".message")[0]);
+
+});
